e58076f0935143bdf70df24c31b6e67299e08e89
Damian Kolasinski <damian.kolasinski@appunite.com>
2020-08-06 17:56:55 +0200

    Added sample

---
+++ KingsPayGS-iOS-Sample/KingsPayGS-iOS-Sample/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  KingsPayGS-iOS-Sample
+//
+//  Created by Damian Kolasiński on 06/08/2020.
+//  Copyright © 2020 KingsChat. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ KingsPayGS-iOS-Sample/KingsPayGS-iOS-Sample/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  KingsPayGS-iOS-Sample
+//
+//  Created by Damian Kolasiński on 06/08/2020.
+//  Copyright © 2020 KingsChat. All rights reserved.
+//
+
+import UIKit
+import KingsPayForGoodsAndServices
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        KingsPayForGoodsAndServices.pay(
+            usingContext: KingsPayForGoodsAndServices.PresentationContext(
+                controller: self,
+                completion: { paymentResult in print(paymentResult) }
+            ),
+            config: KingsPayForGoodsAndServices.PaymentConfig(
+                paymentId: "",
+                clientId: "",
+                environment: .production
+            )
+        )
+    }
+}
